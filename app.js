b9c7e28313750e3e3abb73052a8ecf36c71f8b24
fasbrothers <nigmatovsanjar00@gmail.com>
2021-04-02 22:57:24 +0500

    added validation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const multer = require('multer')
+const { Validator } = require('node-input-validator');
 
 // local library
 const blogs = require('./routes/blogs')
@@ -52,37 +53,40 @@ app.route('/create')
         const description = req.body.description
         const photo = req.file.filename
 
+        const v = new Validator(req.body, {
+            title: 'required|minLength:5|maxLength:50',
+            author: 'required|minLength:5|maxLength:20',
+            description: 'required|minLength:5'
+        });
         
-        if(title.trim() === ""){
-            res.render('create-blog', {error:"Please fill the title!"})
-        }else if(author.length < 5){
-                res.render('create-blog', {error:"Minimum character for author is 5"})
-        }else if(description.trim() === ""){
-            res.render('create-blog', {error:"Please fill the description!"})
-        }
-        else{
-            fs.readFile('./data/blogs.json', (err,data)=>{
-                if(err) res.sendStatus(500)
-                
-                const blogs = JSON.parse(data)
-
-                blogs.push({
-                    id: id(),
-                    title: title,
-                    author:author,
-                    description:description,
-                    photo: photo
-
-                }),
-
-                fs.writeFile('./data/blogs.json', JSON.stringify(blogs), err=>{
+        v.check().then((matched) => {
+            if (!matched) {
+                res.render('create-blog', {error:"Please fill all fields!"})
+            }
+            else{
+                fs.readFile('./data/blogs.json', (err,data)=>{
                     if(err) res.sendStatus(500)
-
-                    res.render('create-blog', {success:true})
+                    
+                    const blogs = JSON.parse(data)
+                    blogs.push({
+                        id: id(),
+                        title: title,
+                        author:author,
+                        description:description,
+                        photo: photo
+                    }),
+                    fs.writeFile('./data/blogs.json', JSON.stringify(blogs), err=>{
+                        if(err) res.sendStatus(500)
+      
+                        res.render('create-blog', {success:true})
+                    })
                 })
-            })
+
+            }
+            });
+        
         }
-    })
+    )
 
 app.use(function(req, res, next){
     res.status(404).render("error")
